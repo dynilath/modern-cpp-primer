df56b6df96d120eba0451dd93e86dc025e231596
dynilath <dynilath@qq.com>
2025-02-21 18:42:23 +0800

    chore: fix dependency and theme setting

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -2,8 +2,6 @@ import { hopeTheme } from "vuepress-theme-hope";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-
-  iconAssets: "fontawesome-with-brands",
   repo: "dynilath/moder-cpp-primer",
 
   docsDir: "src",
@@ -34,49 +32,53 @@ export default hopeTheme({
       components: ["Badge", "VPCard"],
     },
     redirect: {
-      autoLocale: true
+      autoLocale: true,
     },
-    shiki: {
+    icon: {
+      assets: "fontawesome-with-brands",
+    },
+  },
+  markdown: {
+    align: true,
+    attrs: true,
+    codeTabs: true,
+    component: true,
+    demo: true,
+    figure: true,
+    imgLazyload: true,
+    imgSize: true,
+    include: true,
+    mark: true,
+    stylize: [
+      {
+        matcher: "Recommended",
+        replacer: ({ tag }) => {
+          if (tag === "em")
+            return {
+              tag: "Badge",
+              attrs: { type: "tip" },
+              content: "Recommended",
+            };
+        },
+      },
+    ],
+    sub: true,
+    sup: true,
+    tabs: true,
+    vPre: true,
+    mermaid: true,
+    markmap: true,
+    revealjs: {
+      plugins: ["highlight", "math", "search", "notes", "zoom"],
+    },
+    highlighter: {
+      type: "shiki",
       themes: { light: "github-light", dark: "github-dark" },
       langs: ["cpp", "powershell", "bash", "cmd"],
       notationDiff: true,
       notationHighlight: true,
       notationErrorLevel: true,
       notationWordHighlight: true,
-    },
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      codetabs: true,
-      component: true,
-      demo: true,
-      figure: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      mark: true,
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-      mermaid: true,
-      markmap: true,
-      revealJs: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
-      },
     },
   },
 });
